e17dfcbb74b9cebd2e510c8506870806083ab8d4
U-Gusteau\Graeme <Graeme@Gusteau.(none)>
2014-03-21 21:34:47 +0200

    Better formatting

--- torrentWatcher.sh
+++ torrentWatcher.sh
@@ -9,31 +9,31 @@ while [ 1 ]; do
   date
 
   if [ ! -x $HOME/ignore ]; then 
+    
+    ACCOUNT=`wget -O - http://192.168.1.3/getActiveAccount 2>/dev/null`
 
-  ACCOUNT=`wget -O - http://192.168.1.3/getActiveAccount 2>/dev/null`
+    echo Active account is $ACCOUNT
 
-  echo Active account is $ACCOUNT
+    if [ "$ACCOUNT" != "uncapped" ]; then
 
-  if [ "$ACCOUNT" != "uncapped" ]; then
+      if $HOME/pslist.exe | grep -i utorrent; then
 
-    if $HOME/pslist.exe | grep -i utorrent; then
+        echo Found torrent process, trying to close
 
-      echo Found torrent process, trying to close
+        $HOME/closeTorrent.exe
+        sleep 10
 
-      $HOME/closeTorrent.exe
-      sleep 10
+      fi
+
+      echo Killing torrent process
+      $HOME/pskill.exe utorrent
+
+    else
+
+      echo Starting torrent client
+      /cygdrive/c/Users/Graeme/AppData/Roaming/uTorrent/uTorrent.exe
 
     fi
-
-    echo Killing torrent process
-    $HOME/pskill.exe utorrent
-
-  else
-
-    echo Starting torrent client
-    /cygdrive/c/Users/Graeme/AppData/Roaming/uTorrent/uTorrent.exe
-
-  fi
 
   else
 
