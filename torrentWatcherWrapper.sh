58476ffd58376d079bd8abebe575e607ad831895
Graeme Pyle <graeme@graemep.me>
2014-05-04 01:07:38 +0200

    Loop in case node crashes

--- torrentWatcherWrapper.sh
+++ torrentWatcherWrapper.sh
@@ -7,4 +7,11 @@ HOME=`dirname $0`
 #$HOME/torrentWatcher.sh > out.log 2> err.log
 
 cd $HOME
-"/cygdrive/c/Program Files/nodejs/node.exe" torrentWatcher.js > out.log 2> err.log
+
+while [ 1 ]; do
+
+    "/cygdrive/c/Program Files/nodejs/node.exe" torrentWatcher.js >> out.log 2>> err.log
+    echo Dead - waiting 10s before restarting
+    sleep 10
+
+done
